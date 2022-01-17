6d3d1f695ea21d505c359bd459130230196056c7
AchmadZeinFeroza <uchihaferoza22@gmail.com>
2022-01-17 23:35:17 +0700

    ubah kualitas gambar

--- app/Http/Controllers/GambarController.php
+++ app/Http/Controllers/GambarController.php
@@ -41,7 +41,7 @@ class GambarController extends Controller
             $imagePath = $img;
             $image = \Image::make($img);
             $jalur = time().'-'.$imagePath->getClientOriginalName();
-            $path = $image->save(storage_path('app/public/rumah/'.$jalur), 10);
+            $path = $image->save(storage_path('app/public/rumah/'.$jalur), 50);
             $gambar->gambar = '/storage/rumah/'.$jalur;
             $gambar->save();
         }
